886bcb1bcee1dbb7307b895bb99320fbba63e274
Shiananmiad Bermudez <bermudezdamian7@gmail.com>
2022-04-23 17:48:13 -0300

    mejoras en la UI y agregado de cv

--- App.js
+++ App.js
@@ -164,9 +164,28 @@ function slider() {
       nextRight();
     }, 5000);*/
 }
+function enviarFormulario() {
+  const $form = document.querySelector("#formulario");
+  const $buttonEnviar = document.querySelector("#enviar-mail");
+  $form.addEventListener("submit", handleSubmit);
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = new FormData(this);
+    $buttonEnviar.setAttribute(
+      "href",
+      `mailto:bermudezdamian7@gmail.com?subject= Nombre:${form.get(
+        "name"
+      )} Email:${form.get("mail")} Telefono:${form.get(
+        "phone"
+      )} Asunto:${form.get("asunto")}&body=${form.get("message")} `
+    );
+    $buttonEnviar.click();
+  }
+}
 
 function main() {
   menu();
+  enviarFormulario();
   scrolTop();
   slider();
 }
